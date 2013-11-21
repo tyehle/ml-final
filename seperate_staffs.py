aac9671e26d3e175c8fa46c313865c357f6fd515
Eric Hurst <minkusanderic@gmail.com>
2013-11-21 16:47:57 -0700

    cleaned up seperate staffs code

--- seperate_staffs.py
+++ seperate_staffs.py
@@ -1,58 +1,51 @@
 #!/usr/bin/env python
-
-
 from scipy import ndimage
 from scipy import misc
 import matplotlib.pyplot as plt
 import numpy
 import sys
 
-image = misc.imread(sys.argv[1])
+def seperate_staffs(image):
 
+    #filter out any small noise and force any gray in the image to be
+    #pure black or white depending if it is above or below the mean of the image
+    image = (image > image.mean()).astype(numpy.float)
 
-image = (image > image.mean()).astype(numpy.float)
+    #count up the whitespace in each row
+    whitespace_counts = []
+    for i in image:
+        sum = 0
+        for j in i:
+            sum = sum + j
+        whitespace_counts.append(sum[0])
+            
+    #split the image based on whitespace
+    in_image = False
+    images = []
+    threshold = image.shape[1] - 5;
+    row_number = 0
+    for row in whitespace_counts:
+        if in_image:
+            if row > threshold:
+                in_image = False
+                images[-1] = (images[-1][0], row_number)
+        else:
+            if row < threshold:
+                in_image = True
+                images.append((row_number, 0))
+        row_number = row_number + 1
 
-plt.imshow(image)
-plt.show()
+    seperated_images = [image[x[0]:x[1], :] for x in images]
 
-hist = numpy.histogram(image)
-plt.hist(hist)
-plt.show()
+    return seperated_images
 
-
-whitespace_counts = []
-for i in image:
-    sum = 0
-    for j in i:
-        sum = sum + j
-    whitespace_counts.append(sum[0])
-
-plt.plot(whitespace_counts)
-plt.plot([image.mean() * image.shape[1] for x in range(0,image.shape[0])])
-plt.show()
-        
-
-in_image = False
-images = []
-threshold = image.shape[1] - 5;
-row_number = 0
-for row in whitespace_counts:
-    if in_image:
-        if row > threshold:
-            in_image = False
-            images[-1] = (images[-1][0], row_number)
-    else:
-        if row < threshold:
-            in_image = True
-            images.append((row_number, 0))
-    row_number = row_number + 1
-
-print images
-
-seperated_images = [image[x[0]:x[1], :] for x in images]
-
-for i in seperated_images:
-    plt.imshow(i)
+if __name__ == "__main__":
+    image = misc.imread(sys.argv[1])
+    plt.imshow(image)
     plt.show()
-#plt.imshow(hist, cmap=plt.cm.gray)
-#plt.show()
+
+    staffs = seperate_staffs(image)
+    for i in staffs:
+        plt.imshow(i)
+        plt.show()
+
