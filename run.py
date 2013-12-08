fa307df5fda23ee2e49abb1edb271f1d2681122b
Tobin Yehle <tobinyehle@gmail.com>
2013-12-08 16:17:40 -0700

    accuracy testing data

--- run.py
+++ run.py
@@ -30,17 +30,60 @@ def center(image):
     loc = 0
     for i in range(len(row_brightness)):
         loc = loc + i * row_brightness[i]
-    return loc / len(row_brightness)**2
+    return numpy.array([loc / len(row_brightness)**2])
 
 
-def get_type_classifier(file_location, get_features):
-    """ Gets a classifier that can be used to find the type of a note. """
+def get_type_classifier_subset(train_ratio, file_location, get_features):
+    print("-- Training Type Classifier --")
     file_data = open(file_location, 'r')
     image_files = json.load(file_data)
 
+    notes = []
+    # filter out anything that isn't a note
+    for image in image_files:
+        file_name = image.keys()[0]
+        data = image[file_name]
+        if data["type"] not in ["ignore", "time", "bar"]:
+            notes.append(image)
+
+    # separate out images to test on
+    test_images = []
+    for _ in range(int((1.0 - train_ratio) * len(notes))):
+        index = int(random.random() * len(notes))
+        test_images.append(notes.pop(index))
+
+    print("training on %i images" % len(notes))
+    print("testing on %i images" % len(test_images))
+
+    (classer, label_map) = get_type_classifier(notes, get_features, verbose=True)
+
+    num_correct = 0
+    for image in test_images:
+        file_name = image.keys()[0]
+        data = image[file_name]
+        if data["type"] == "note":
+            expected_label = "note,{0}".format(data["length"])
+        elif data["type"] == "rest":
+            expected_label = "rest,{0}".format(data["length"])
+        else:
+            expected_label = data["type"]
+        features = get_features(misc.imread(file_name))
+        actual_label = label_map[classer(features)]
+        if actual_label == expected_label:
+            num_correct = num_correct + 1
+        else:
+            print("Features")
+            print(features)
+            print("Expected %s, got %s" % (expected_label, actual_label))
+
+    print("Accuracy %f (%i / %i)" % ((float(num_correct) / len(test_images)), num_correct, len(test_images)))
+
+
+def get_type_classifier(image_files, get_features, verbose=False):
+    """ Gets a classifier that can be used to find the type of a note. """
     label_map = []
     labels = []
-    features = []
+    file_names = []
 
     # Iterating through each image that the algorithm can train on.
     for image in image_files:
@@ -65,35 +108,61 @@ def get_type_classifier(file_location, get_features):
 
         # add the label for this image to the list of labels
         labels.append(index)
+        file_names.append(file_name)
 
-        # add the feature vector for this image to the list of data
-        image = misc.imread(file_name)
-        features.append(get_features(image))
-
-    # stack the data correctly
-    features = numpy.vstack(features).T
-
-    print(features)
-
-    # get the classifier
-    (classer, phi, mu, sig) = clustering.gda(features, labels, len(label_map))
-    print("label map")
-    print(label_map)
-    print("phi")
-    print(phi)
-    print("mu")
-    print(mu)
+    classer = train_classifier(file_names, labels, len(label_map), get_features, verbose)
+    if verbose:
+        print("Label map:")
+        print(label_map)
     return classer, label_map
 
 
-def get_pitch_classifier(file_location, get_features):
-    """ Gets a classifier that can be used to find the pitch of a note. """
+def get_pitch_classifier_subset(train_ratio, file_location, get_features):
+    print("-- Training Pitch Classifier --")
     file_data = open(file_location, 'r')
     image_files = json.load(file_data)
 
+    notes = []
+    # filter out anything that isn't a note
+    for image in image_files:
+        file_name = image.keys()[0]
+        data = image[file_name]
+        if data["type"] == "note":
+            notes.append(image)
+
+    # separate out images to test on
+    test_images = []
+    for i in range(int((1.0 - train_ratio) * len(notes))):
+        index = int(random.random() * len(notes))
+        test_images.append(notes.pop(index))
+
+    print("training on %i images" % len(notes))
+    print("testing on %i images" % len(test_images))
+
+    (classer, label_map) = get_pitch_classifier(notes, get_features, verbose=True)
+
+    num_correct = 0
+    for image in test_images:
+        file_name = image.keys()[0]
+        data = image[file_name]
+        expected_label = data["pitch"]
+        features = get_features(misc.imread(file_name))
+        actual_label = label_map[classer(features)]
+        if actual_label == expected_label:
+            num_correct = num_correct + 1
+        else:
+            print("Features")
+            print(features)
+            print("Expected %s, got %s" % (expected_label, actual_label))
+
+    print("Accuracy %f (%i / %i)" % ((float(num_correct) / len(test_images)), num_correct, len(test_images)))
+
+
+def get_pitch_classifier(image_files, get_features, verbose=False):
+    """ Gets a classifier that can be used to find the pitch of a note. """
     label_map = []
     labels = []
-    features = []
+    file_names = []
 
     # Iterating through each image that the algorithm can train on.
     for image in image_files:
@@ -114,27 +183,42 @@ def get_pitch_classifier(file_location, get_features):
 
         # add the label for this image to the list of labels
         labels.append(index)
+        file_names.append(file_name)
 
-        # add the feature vector for this image to the list of data
+    classer = train_classifier(file_names, labels, len(label_map), get_features, verbose)
+    if verbose:
+        print("Label map:")
+        print(label_map)
+    return classer, label_map
+
+
+def train_classifier(image_files, labels, m, get_features, verbose=False):
+    """ Trains a classifier using the given images files and labels.
+        Uses the given function to extract a feature vector from the image file.
+    """
+    features = []
+    for file_name in image_files:
         image = misc.imread(file_name)
         features.append(get_features(image))
 
     # stack the data correctly
     features = numpy.vstack(features).T
 
-    print(features)
-
     # get the classifier
-    (classer, phi, mu, sig) = clustering.gda(features, labels, len(label_map))
-    print("label map")
-    print(label_map)
-    print("phi")
-    print(phi)
-    print("mu")
-    print(mu)
-    return classer, label_map
+    (classer, phi, mu, sig) = clustering.gda(features, labels, m)
+    if verbose:
+        print("Data:")
+        print(features)
+        print("phi:")
+        print(phi)
+        print("mu:")
+        print(mu)
+        print("sigma:")
+        print(sig)
+    return classer
 
 
 if __name__ == "__main__":
-    # get_type_classifier("learn/learn.json", lambda img: img.mean())
-    get_pitch_classifier("learn/learn.json", center)
+    train_ratio = .9
+    get_type_classifier_subset(train_ratio, "learn/learn.json", lambda img: numpy.array([img.mean()]))
+    get_pitch_classifier_subset(train_ratio, "learn/learn.json", center)
