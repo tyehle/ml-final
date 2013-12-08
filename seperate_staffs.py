36879bb04d0ff31a6eaa1bb9fca3a12026ab35e8
Eric Hurst <minkusanderic@gmail.com>
2013-12-08 14:47:46 -0700

    added learn script

--- seperate_staffs.py
+++ seperate_staffs.py
@@ -85,48 +85,4 @@ def seperate_notes(image):
     return seperated_images
 
 if __name__ == "__main__":
-    image = misc.imread(sys.argv[1])
-    plt.imshow(image)
-    plt.show()
-
-    image_counter = 0
-    staffs = seperate_staffs(image)
-    results = []
-    
-    for staff in staffs:
-        transposed_staff = ndimage.rotate(staff, -90)
-        notes = seperate_notes(transposed_staff)
-        for i in range(len(notes)):
-            filename = 'learn/{0}.jpg'.format(str(image_counter).zfill(5))
-            misc.imsave(filename, notes[i])
-            image_counter = image_counter + 1
-
-            plt.imshow(ndimage.rotate(notes[i], 90))
-            plt.ion()
-            plt.show()
-            
-            thing = {}
-            
-            note_type = raw_input("Type?: ")
-            while not note_type in ["note", "treble", "ignore", "bass", "time", "bar", "rest"]:
-                note_type = raw_input("Retry Type?: ")
-            thing["type"] = note_type
-
-            if note_type == "note":
-                note_pitch = raw_input("Pitch?: ")
-                thing["pitch"] = int(note_pitch)
-                note_length = raw_input("Length?: ")
-                thing["length"] = float(note_length)
-            if note_type == "rest":
-                note_length = raw_input("Length?: ")
-                thing["length"] = float(note_length)
-            print({filename:thing})
-            results.append({filename:thing})
-            
-            
-    json_result = json.dumps(results)
-    print json_result
-    open("learn/learn.json", 'w').write(json_result)
-            #plt.imshow(note)
-            #plt.show()
-
+    print "do not run this file yet"
